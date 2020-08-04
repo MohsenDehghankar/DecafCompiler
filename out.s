f837aa05421a528cfa3f5adafd73a997226564d0
hamilamailee <hamilamailee@yahoo.com>
2020-08-05 01:27:33 +0430

    _for Debugged

--- out.s
+++ out.s
@@ -16,81 +16,71 @@ la $s1, global_pointer;
 
 
 
-li $v0, 9;
-li $a0, 6;
-syscall
 
 
-li $a0,'s';
-sb $a0,0($v0);
-
-
-li $a0,'a';
-sb $a0,1($v0);
-
-
-li $a0,'l';
-sb $a0,2($v0);
-
-
-li $a0,'a';
-sb $a0,3($v0);
-
-
-li $a0,'m';
-sb $a0,4($v0);
-
-
-lb $a0,end_of_string;
-sb $a0,5($v0);
+li $t0, 0;
 
 
 li $t1, 8;
 add $t1, $t1, $s0;
-sw $v0, ($t1);
+sw $t0, ($t1);
+
+
+    label3:
+
+
+
+
+li $t0, 8;
+add $t0, $t0, $s0;
+lw $t0, ($t0);
+
+
+li $t1, 10;
+
+
+
+blt $t0, $t1, label1;
+
+
+
+
+add $t0, $zero, $zero;
+b label2;
+label1:
+addi $t0, $zero, 1;
+label2:
+
+
+    beq $t0,$zero,label4;
+
 
 
 
 
 li $v0, 9;
-li $a0, 9;
+li $a0, 5;
 syscall
 
 
-li $a0,'s';
+li $a0,'a';
 sb $a0,0($v0);
 
 
-li $a0,'a';
+li $a0,'h';
 sb $a0,1($v0);
 
 
-li $a0,'l';
+li $a0,'a';
 sb $a0,2($v0);
 
 
-li $a0,'a';
+li $a0,'n';
 sb $a0,3($v0);
 
 
-li $a0,'a';
-sb $a0,4($v0);
-
-
-li $a0,'a';
-sb $a0,5($v0);
-
-
-li $a0,'a';
-sb $a0,6($v0);
-
-
-li $a0,'m';
-sb $a0,7($v0);
-
-
 lb $a0,end_of_string;
-sb $a0,8($v0);
+sb $a0,4($v0);
 
 
 move $a0, $v0;
@@ -101,6 +91,33 @@ syscall
 li $v0, 4;
 la $a0, newline;
 syscall
+
+
+
+
+
+li $t1, 8;
+add $t1, $t1, $s0;
+lw $t1, ($t1);
+
+
+li $t2, 1;
+
+
+add $t1, $t1, $t2
+
+
+
+move $t2, $t1;
+
+
+li $t1, 8;
+add $t1, $t1, $s0;
+sw $t2, ($t1);
+
+
+    j label3;
+    label4:
 
 
 li $v0, 10;
