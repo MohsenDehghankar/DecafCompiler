5fe9d4ee97d9545fe7066d4b283f5f2105ebe270
MohsenDehghankar <mohsendehghankar@gmail.com>
2020-08-07 13:38:57 +0430

    ignore out.s

--- out.s
+++ out.s
@@ -1,4 +1,5 @@
 
+
 .data
 frame_pointer:  .space  10000
 global_pointer: .space  10000
@@ -17,117 +18,44 @@ la $s1, global_pointer;
 
 
 
-li $v0, 5;
-syscall
-move $t0, $v0;
-        
+li $t0, 1;
+            
 
-
-move $t1, $t0;
-                
-
-li $t0, 8;
-add $t0, $t0, $s0;
-sw $t1, ($t0);
+li $t1, 8;
+add $t1, $t1, $s0;
+sb $t0, ($t1);
                 
 
 
-li $v0, 5;
-syscall
-move $t0, $v0;
-        
+
+
+li $t0, 0;
+            
 
 li $t1, 9;
 add $t1, $t1, $s0;
 sb $t0, ($t1);
                 
 
-move $t1, $t0;
-                
 
-li $t0, 12;
+
+
+
+
+
+
+li $t0, 8;
 add $t0, $t0, $s0;
-sw $t1, ($t0);
-                
-
-
-
-
-li $t0, 24;
-        
-
-
-li $t1, -4;
-add $t1, $t1, $t0;
+lb $t0, ($t0);
             
 
-lw $t2, 8($s0);
+
+li $t1, 9;
 add $t1, $t1, $s0;
-sw $t2, ($t1);
-                    
-
-
-li $t1, -8;
-add $t1, $t1, $t0;
+lb $t1, ($t1);
             
 
-lw $t2, 12($s0);
-add $t1, $t1, $s0;
-sw $t2, ($t1);
-                    
-
-li $t2, 24;
-add $t2, $t2, $s0;
-sw $s0, ($t2);
-        
-
-move $t2, $s0;
-add $t2, $t2, 24
-move $s0, $t2;
-jal abs_mult;
-        
-
-move $t2, $v0;
-        
-
-li $v0, 1;
-move $a0, $t2;
-syscall
-                        
-
-li $v0, 4;
-la $a0, newline;
-syscall
-        
-
-li $v0, 10;
-syscall;
-            
-
-abs_mult:
-
-sw $ra, 4($s0);
-        
-
-
-
-
-
-
-
-
-
-li $t0, -4;
-add $t0, $t0, $s0;
-lw $t0, ($t0);
-            
-
-li $t1, -8;
-add $t1, $t1, $s0;
-lw $t1, ($t1);
-            
-
-bgt $t0, $t1, label1;
+bne $t0, $t1, label1;
         
         
 
@@ -140,105 +68,20 @@ label2:
         
 
 beq $t0, $zero, label3;
-        
-
-
-
-
-
-li $t1, -4;
-add $t1, $t1, $s0;
-lw $t1, ($t1);
-                    
-
-
-li $t2, -8;
-add $t2, $t2, $s0;
-lw $t2, ($t2);
-                    
-
-sub $t1, $t1, $t2
-        
-
-
-move $t2, $t1;
-                
-
-li $t1, 8;
-add $t1, $t1, $s0;
-sw $t2, ($t1);
-                
-
-b label4;
+la $a0, true_const;
+j label4;
 label3:
-        
-
-
-
-
-
-li $t1, -8;
-add $t1, $t1, $s0;
-lw $t1, ($t1);
-                    
-
-
-li $t2, -4;
-add $t2, $t2, $s0;
-lw $t2, ($t2);
-                    
-
-sub $t1, $t1, $t2
-        
-
-
-move $t2, $t1;
-                
-
-li $t1, 8;
-add $t1, $t1, $s0;
-sw $t2, ($t1);
-                
-
+la $a0, false_const;
 label4:
+li $v0, 4;
+syscall
+                    
+
+li $v0, 4;
+la $a0, newline;
+syscall
         
 
-
-
-
-
-
-li $t1, 8;
-add $t1, $t1, $s0;
-lw $t1, ($t1);
-                    
-
-
-li $t2, -4;
-add $t2, $t2, $s0;
-lw $t2, ($t2);
-                    
-
-mult $t1, $t2;
-mflo $t1;
+li $v0, 10;
+syscall;
             
-
-
-
-move $t2, $t1;
-                
-
-
-li $t3, -8;
-add $t3, $t3, $s0;
-lw $t3, ($t3);
-                    
-
-mult $t2, $t3;
-mflo $t2;
-            
-lw $ra, 4($s0);
-move $v0, $t2;
-lw $s0, ($s0);
-jr $ra;
-                
